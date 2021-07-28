8962c48a093b5832fc47083edeaa759e0ddb1ec7
PeterWuMQ <80020886+PeterWuMQ@users.noreply.github.com>
2021-07-28 18:57:32 +1000

    Added OnMouseDown and Collider

--- COMP2160 Prac Week 1/Assets/Scripts/Mole.cs
+++ COMP2160 Prac Week 1/Assets/Scripts/Mole.cs
@@ -3,20 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Collider))]
 
 public class Mole : MonoBehaviour
 {
-    public Color color = Color.red;
+    public Color InitialColor = Color.red;
+    public Color ChangedColor = Color.black;
+    SpriteRenderer sprite;
+
     // Start is called before the first frame update
     void Start()
     {
-        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
-        sprite.color = color;
+        sprite = GetComponent<SpriteRenderer>();
+        sprite.color = InitialColor;
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    void OnMouseDown() 
+    {
+        sprite.color = ChangedColor;
     }
 }
