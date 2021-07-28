43cb46a173b14dcc1f52777fcd283c983df7d619
PeterWuMQ <80020886+PeterWuMQ@users.noreply.github.com>
2021-07-28 18:34:14 +1000

    Fixed Delta and Created GameObject

---
+++ COMP2160 Prac Week 1/Assets/Scripts/Mole.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
