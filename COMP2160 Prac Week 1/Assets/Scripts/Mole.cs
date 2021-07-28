519efd419eabcf4d23bf125ea34057a98375c429
PeterWuMQ <80020886+PeterWuMQ@users.noreply.github.com>
2021-07-28 18:46:07 +1000

    Circle and Color Added

--- COMP2160 Prac Week 1/Assets/Scripts/Mole.cs
+++ COMP2160 Prac Week 1/Assets/Scripts/Mole.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 [RequireComponent(typeof(SpriteRenderer))]
 
-public class NewBehaviourScript : MonoBehaviour
+public class Mole : MonoBehaviour
 {
+    public Color color = Color.red;
     // Start is called before the first frame update
     void Start()
     {
         SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        sprite.color = color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
